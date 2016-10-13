46685d9dc2e3c12f77ebe250cfb09482575b1a44
Benson Kimani <wbensonkimani@gmail.com>
2016-10-13 13:17:34 +0300

    Update alexxa.py

--- alexxa.py
+++ alexxa.py
@@ -3,3 +3,20 @@
 
 class alexxa():
   pass
+
+
+
+
+
+def add_entry(self):
+
+    """add a journal entry"""
+	
+    print('Please Enter your journal entry.')
+	
+    data = sys.stdin.read().strip()
+
+    if data:
+        if input('Save entry? Y/N').lower() != 'n':
+            Entry.create(content=data)
+            print('Saved!')
