df12f61873eddf1fc05c5d1aaf98187a7dff9312
Gilbert <gilugates@gmail.com>
2016-10-13 14:53:36 +0300

    Gilbert: Added the first iteration of the main function.

--- alexxa.py
+++ alexxa.py
@@ -50,3 +50,56 @@ def get_last_journal(username):
 
 	return result
 
+
+# username can be changed by user at command line
+global username
+
+if __name__ == '__main__':
+  print "Enter username to access journal:"
+  username = input()
+
+  journal_count = get_user_journals(username)
+  last_journal = get_last_journal(username)
+
+  # could be -1 or user's name
+  welcome = get_user_data(username)
+  if welcome != -1:
+    # user has been found
+    print "Welcome %s" %username+ ". You have {:d}".format(journal_count)+" journals. Last entered journal is: %s" %last_journal
+  else:
+    # user not found
+    print "User not found. Try another username."
+
+  while user_input != 3:
+    print "Press 1 to 'create new journal', 2 to 'change username', or 3 to 'quit'."
+    user_input = input()
+    system_response = handle_user_input(user_input)
+
+    if system_response == 1:
+      # get journal title
+      print "Enter journal title:"
+      title = input()
+
+      # get journal content
+      print "Enter journal content:"
+      content = input()
+
+      # response
+      response = create_new_journal(username,title,content)
+      if response == 1:
+        print "Successfully entered journal. You have {:d}".format(journal_count)+" journals. Last entered journal is %s" %last_journal
+      else:
+        print "Error encountered while creating journal."
+    elif system_response == 0:
+      print "Enter the username to change to:"
+      change_username = input()
+
+      # change the global var to match new username request
+      username = change_username
+
+      # successfully changed the user
+      print "Welcome %s" %username+ ". You have {:d}".format(journal_count)+" journals. Last entered journal is: %s" %last_journal
+
+    else:
+      # should be returned when the app is quitting
+      print "Thank you for using the Alexxa app :)"
