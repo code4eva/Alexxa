ea6b0d49d520f16439feeda97df21f27688683fa
code4eva <thomasnzioki07@gmail.com>
2016-10-13 15:32:52 +0300

    Final commit

--- alexxa.py
+++ alexxa.py
@@ -1,100 +1,42 @@
-#! usr/bin/env Python3
+import sys
 
-# saves user data: title and content
-def add_entry(data):
+def create_new_journal(user, title, content):
+	dUser  = user
+	dTitle = title
+	dContent = content
 
-    """add a journal entry"""
-    input =[]
-	
-    #print('Please Enter your journal.')
-	
-    #data = sys.stdin.read().strip()
+	data = {dUser:[(dTitle,dContent)]}
+	return data
 
-    if data:
-        
-     	if input('Save entry? Y/N').lower() != 'n':
-            
-    #Entry.create(content=data)
-        new_journal=input.append(data)
-    result = "journal succesfully saved"
-
-           # print('Saved!')
-    else:
-
-    	result = "No journal entry"
-
-    return result
-
-
-
-#print(add_entry(""))
-
-def get_last_journal(username):
-	#dummy journal list
-	journal_list = ["yes", "no", "jinx"]
-	#user is active
-	if username == "Ray":
-
-		result = journal_list[-1]
-	#user doesnt exist
+def get_user_data(data, username):
+	if username in data.keys():
+		return True
 	else:
-
-		result = "User does not exist"
-
-	return result
+		return False
 
 
-# username can be changed by user at command line
-global username
+def get_user_journal(data,username):
+	return len(data[username])
+
+def get_last_journal(data, username):
+	return data[username][-1]
 
 if __name__ == '__main__':
-  print "Enter username to access journal:"
-  username = raw_input()
 
-  # number of journals created by the user
-  journal_count = get_user_journals(username)
-  # last created journal by the user
-  last_journal = get_last_journal(username)
+	data = {}
+	
+	if not data:
+		try:
+			user = sys.argv[1]
+		except:
+			user  = input('Enter your username\n')
+	title   = input('Enter title of your article\n')
+	content = input('Enter journal\n')
+	data = create_new_journal(user, title, content)
+	#print(data)
+	print('Welcome  ', user, 'this is Alexxa')
+	print('Title : ', data[user][-1][0] )
+	print('Journal Entry : ', data[user][-1][1])
 
-  # could be -1 or user's name
-  welcome = get_user_data(username)
-  if welcome != -1:
-    # user has been found
-    print "Welcome %s" %username+ ". You have {:d}".format(journal_count)+" journals. Last entered journal is: %s" %last_journal
-  else:
-    # user not found
-    print "User not found. Try another username."
 
-  while user_input != 3:
-    print "Press 1 to 'create new journal', 2 to 'change username', or 3 to 'quit'."
-    user_input = raw_input()
-    system_response = handle_user_input(user_input)
 
-    if system_response == 1:
-      # get journal title
-      print "Enter journal title:"
-      title = raw_input()
-
-      # get journal content
-      print "Enter journal content:"
-      content = raw_input()
-
-      # response
-      response = create_new_journal(username,title,content)
-      if response == 1:
-        print "Successfully entered journal. You have {:d}".format(journal_count)+" journals. Last entered journal is %s" %last_journal
-      else:
-        print "Error encountered while creating journal."
-    elif system_response == 0:
-      print "Enter the username to change to:"
-      change_username = raw_input()
-
-      # change the global var to match new username request
-      username = change_username
-
-      # successfully changed the user
-      print "Welcome %s" %username+ ". You have {:d}".format(journal_count)+" journals. Last entered journal is: %s" %last_journal
-
-    else:
-      # should be returned when the app is quitting
-      print "Thank you for using the Alexxa app :)"
